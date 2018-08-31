8f92d33f7d473ec49a730fcb24f6ee31573218b4
root <upday7@163.com>
2018-08-31 16:31:43 +0800

    init

---
+++ ReviewerContextMenu/__init__.py
@@ -0,0 +1,36 @@
+from anki.hooks import addHook
+from aqt import mw
+
+
+def _reviewerContextMenu(view, menu):
+    if mw.state != "review":
+        return
+
+    self = mw.reviewer
+    opts = [
+        [_("Flag Card"), [
+            [_("Red Flag"), "Ctrl+1", lambda: self.setFlag(1)],
+            [_("Purple Flag"), "Ctrl+2", lambda: self.setFlag(2)],
+            [_("Green Flag"), "Ctrl+3", lambda: self.setFlag(3)],
+            [_("Blue Flag"), "Ctrl+4", lambda: self.setFlag(4)],
+            None,
+            [_("Clear Flag"), "Ctrl+0", lambda: self.setFlag(0)],
+        ]],
+        [_("Mark Note"), "*", self.onMark],
+        [_("Bury Card"), "-", self.onBuryCard],
+        [_("Bury Note"), "=", self.onBuryNote],
+        [_("Suspend Card"), "@", self.onSuspendCard],
+        [_("Suspend Note"), "!", self.onSuspend],
+        [_("Delete Note"), "Ctrl+Delete", self.onDelete],
+        [_("Options"), "O", self.onOptions],
+        None,
+        [_("Replay Audio"), "R", self.replayAudio],
+        [_("Record Own Voice"), "Shift+V", self.onRecordVoice],
+        [_("Replay Own Voice"), "V", self.onReplayRecorded],
+    ]
+
+    self._addMenuItems(menu, [None, ])
+    self._addMenuItems(menu, opts)
+
+
+addHook('AnkiWebView.contextMenuEvent', _reviewerContextMenu)
